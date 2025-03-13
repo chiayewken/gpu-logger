ee01ca33fdee870a6cfe147c52f65dd3cbcf54c4
chiayewken <chiayewken@gmail.com>
2025-03-13 18:41:47 +0800

    Update requirements.txt and main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 Log gpu usage statistics at regular intervals
 Adapted from: https://github.com/fbcotter/py3nvml/blob/0.2.5/scripts/py3smi
 """
+
+import json
 import os
 import pwd
 import time
+from pathlib import Path
 from subprocess import Popen, PIPE
 from typing import List
 
+import matplotlib.pyplot as plt
 from fire import Fire
 from py3nvml.py3nvml import (
     nvmlDeviceGetHandleByIndex,
@@ -21,6 +25,7 @@ from py3nvml.py3nvml import (
     nvmlDeviceGetName,
 )
 from pydantic import BaseModel
+from tqdm import tqdm
 
 
 class Device(BaseModel):
@@ -61,17 +66,19 @@ def get_uname_pid(pid):
         uid = proc_stat_file.st_uid
         # # look up the username from uid
         username = pwd.getpwuid(uid)[0]
-    except:
+    except Exception as e:
+        print(e)
         username = "???"
     return username
 
 
-def get_pname(id):
+def get_pname(i):
     try:
-        sess = Popen(["ps", "-o", "cmd=", "{}".format(id)], stdout=PIPE, stderr=PIPE)
+        sess = Popen(["ps", "-o", "cmd=", "{}".format(i)], stdout=PIPE, stderr=PIPE)
         stdout, stderr = sess.communicate()
         name = stdout.decode("utf-8").strip()
-    except:
+    except Exception as e:
+        print(e)
         name = ""
     return name
 
@@ -80,10 +87,11 @@ def get_uptime(pid):
     try:
         sess = Popen(["ps", "-q", str(pid), "-o", "etime="], stdout=PIPE, stderr=PIPE)
         stdout, stderr = sess.communicate()
-        time = stdout.decode("utf-8").strip()
-    except:
-        time = "?"
-    return time
+        uptime = stdout.decode("utf-8").strip()
+    except Exception as e:
+        print(e)
+        uptime = "?"
+    return uptime
 
 
 def get_processes(device: Device) -> List[Process]:
@@ -91,7 +99,7 @@ def get_processes(device: Device) -> List[Process]:
     procs = []
 
     min_number = nvmlDeviceGetMinorNumber(h)
-    for raw in nvmlDeviceGetComputeRunningProcesses((h)):
+    for raw in nvmlDeviceGetComputeRunningProcesses(h):
         p = Process(
             device_id=min_number,
             user=get_uname_pid(raw.pid),
@@ -114,7 +122,7 @@ class NvmlContext:
         self.num_gpus = nvmlDeviceGetCount()
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, exit_type, value, traceback):
         nvmlShutdown()
 
 
@@ -127,7 +135,7 @@ def test_get_device():
     with NvmlContext() as c:
         for i in range(c.num_gpus):
             device = get_device(i)
-            print(device.json(indent=2))
+            print(device.model_dump_json(indent=2))
 
 
 def test_get_process():
@@ -135,7 +143,7 @@ def test_get_process():
         for i in range(c.num_gpus):
             device = get_device(i)
             for process in get_processes(device):
-                print(process.json(indent=2))
+                print(process.model_dump_json(indent=2))
 
 
 class Record(BaseModel):
@@ -143,21 +151,55 @@ class Record(BaseModel):
     devices: List[Device]
     processes: List[Process]
 
+    def get_device(self, i: int) -> Device:
+        mapping = {device.id: device for device in self.devices}
+        return mapping.get(i)
 
-def main(path_out: str, interval: int):
-    while True:
+
+class LogData(BaseModel):
+    records: List[Record]
+
+    def save(self, path: str):
+        Path(path).parent.mkdir(parents=True, exist_ok=True)
+        with open(path, "w") as f:
+            for record in self.records:
+                f.write(record.model_dump_json() + "\n")
+
+    @classmethod
+    def load(cls, path: str):
+        with open(path, "r") as f:
+            records = [Record(**json.loads(line)) for line in f.readlines()]
+        return cls(records=records)
+
+
+def save_logs(path_out: str, interval: int):
+    data = LogData(records=[])
+    for _ in tqdm(range(int(1e9))):
         with NvmlContext() as c:
             devices = [get_device(i) for i in range(c.num_gpus)]
-            record = Record(
-                time=time.time(),
-                devices=devices,
-                processes=[p for d in devices for p in get_processes(d)],
+            data.records.append(
+                Record(
+                    time=time.time(),
+                    devices=devices,
+                    processes=[p for d in devices for p in get_processes(d)],
+                )
             )
 
-            with open(path_out, "a") as f:
-                f.write(record.json() + "\n")
+        data.save(path_out)
         time.sleep(interval)
 
+
+def visualize_logs(path: str, device: int, path_out: str):
+    data = LogData.load(path)
+    values = [record.get_device(device).mem_used for record in data.records]
+    plt.plot(values)
+    plt.savefig(path_out)
+
+
+"""
+python main.py save_logs logs.jsonl --interval 1
+python main.py visualize_logs logs.jsonl --device 1 --path_out gpu_memory.png
+"""
 
 if __name__ == "__main__":
     Fire()
